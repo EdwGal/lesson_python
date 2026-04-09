477b8e0c54424a327e36e79b84c409dd186cfc4f
EdwGal <galvezgutierrez@gmail.com>
2026-04-09 17:23:42 -0500

    act executablea

--- bucles.py
+++ bucles.py
@@ -2,6 +2,7 @@ op = "s"
 i = True
 CONSTANTEEJE = 0.0062
 CONSTANTEPL = 7.85
+PI = 3.1416
 IGV = 0.18
 def pesobarra(d1, long1):
     peso_barra = d1*d1*CONSTANTEEJE*long1
@@ -21,27 +22,40 @@ while  i == True:
     print("====================================")
     print("1. PESO DE UNA BARRA")
     print("2. PESO DE UNA PLANCHA")
-    print("3. PESO DE VIGAS Y CANALES")
+    print("3. PESO BARRA PERFORADA")
     print("4. PESO DE BARRAS CUADRADAS")
     print("5. IGV")
     print("6. PRECIO CON MARGEN")
-    print("7. SALIR")
-    opc = int (input("escoje la opcion :"))
+    print("7. PESO DE VIGAS Y CANALES")
+    print("8. AUMENTO DE PRECIO PORCENTUAL")
+    print("9. SALIR")
+    opc = int (input("Escoje la opcion : "))
     if opc == 1:
-        diametro_1 = float(input('Ingrese diametro: '))
-        longitud = float(input('Ingrese longitud: '))
+        diametro_1 = float(input('Ingrese Diametro: '))
+        longitud = float(input('Ingrese Longitud: '))
         print("PESO DE LA BARRA ES: ", round(pesobarra(diametro_1,longitud),4),"kgs", "\n")
     elif opc == 2:
-        espesor = float(input('ingrese el espesor de la plancha: '))
-        ancho = float(input('ingrese el ancho:'))
-        largo = float(input('ingres el largo:'))
+        espesor = float(input('Ingrese el Espesor de la Plancha: '))
+        ancho = float(input('Ingrese el Ancho:'))
+        largo = float(input('Ingrese el Largo:'))
         print('PESO DE LA PLANCHA ES: ', round(pesoplancha(espesor,ancho,largo), 4),'kgs','\n')
     elif opc == 3:
         print("estamos trabajando en el modulo")
+        d_Ext = float(input('Ingrese el Diametro Exterior: '))
+        d_Int = float(input('Ingrese el Diametro Interior: ')) 
+        longitud = float(input('Ingrese Longitud: '))
+        area  = (PI/4)*(d_Ext*d_Ext - d_Int*d_Int)
+        print("AREA ES: ", round(area,4),"cm2", "\n")
+        volumen = area * longitud
+        print("VOLUMEN ES: ", round(volumen,4),"cm3", "\n")
+        peso = (volumen * CONSTANTEPL)/1000
+        print("PESO DE LA BARRA PERFORADA ES: ", round(peso,4),"kgs", "\n") 
+        
+        
     elif opc == 4:
         print("estamos trabajando en el modulo")
     elif opc == 5:
-        PreTotal = float(input('ingrese el precio total: '))
+        PreTotal = float(input('Ingrese Precio Total: '))
         valores = igv(PreTotal)
         print("************************")
         print("SUB-TOTAL: ",round(valores[0],2))
@@ -51,16 +65,23 @@ while  i == True:
         #largo = float(input('ingres el largo:'))
         #print('El peso de laplancha es de : ', round(pesoplancha(espesor,ancho,largo), 4),'kgs','\n')
     elif opc == 6:
-        PrecProvedor = float(input('ingrese el precio del material del  proveedor: '))
-        PesoMaterial = float(input('ingrese el peso del Material:'))
-        ValMargen = float(input('ingres el Margen a calcular:'))
+        PrecProvedor = float(input('Ingrese Precio del Material del Proveedor: '))
+        PesoMaterial = float(input('Ingrese Peso del Material:'))
+        ValMargen = float(input('Ingrese el Margen a Calcular:'))
         ValMargen = ValMargen/100
         ValMargen = 1- ValMargen
-        print('precio x kilo proveedor', PrecProvedor/PesoMaterial)
-        print('precio con margen ', ((PrecProvedor/PesoMaterial)/ValMargen))
+        print('Precio x Kilo Proveedor', PrecProvedor/PesoMaterial)
+        print('Precio con Margen ', ((PrecProvedor/PesoMaterial)/ValMargen))
         PrecioFinal = (PrecProvedor/PesoMaterial)/(ValMargen)
         print('El precio con un marge de', ValMargen , 'es : ', round((PrecioFinal), 4),'kgs','\n')
-    elif opc ==7:
+    elif opc == 7:
+        print("estamos trabajando en el modulo")
+    elif opc == 8:
+        pre_ant = float(input('Ingrese el Precio Anterior: '))
+        pre_act = float(input('Ingrese el Precio Actual: '))
+        aumento = ((pre_act - pre_ant) / pre_ant) * 100
+        print('El aumento porcentual es de: ', round(aumento, 2), '%', '\n')
+    elif opc == 9:
         op = str(input("DESEA SALIR DEL APP s/n :"))
         if op == "s":
             i=False
